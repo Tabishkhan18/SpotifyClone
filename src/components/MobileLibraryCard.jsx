c9804ab5c699a1d708a607844166b386845cb5c6
Tabishkhan18 <tabishkhan1811@gmail.com>
2024-08-20 15:59:15 +0530

    update files

--- src/components/MobileLibraryCard.jsx
+++ src/components/MobileLibraryCard.jsx
@@ -5,7 +5,7 @@ const MobileLibraryCard = ({ imageName, title }) => {
     return (
         <button className='cursor-pointer bg-neutral-800 rounded-md w-full'>
             <div className="librarycard flex">
-                <img className='size-12 rounded-s-md' src={imagePath} alt="Song Image" />
+                <img className='size-14 rounded-s-md' src={imagePath} alt="Song Image" />
                 <div className="librarytext flex flex-col items-start justify-center ms-2">
                     <h3 className="librarytitle text-white font-semibold text-sm">{title}</h3>
                 </div>
